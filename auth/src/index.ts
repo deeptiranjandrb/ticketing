e3fc5fbbd95ccb0ffcc095bff252288bc7816e9e
dbali00 <dbali00@bhnetwork.com>
2021-12-09 21:00:30 +0530

    completed auth module

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,46 @@
 import express from 'express';
-
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import { json } from 'body-parser';
-
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 const app = express();
-app.use(json());
 
-app.get('/api/users/currentuser',(req,res) => {
-    res.send('Hi there!');
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.all('*',() => {
+    throw new NotFoundError();
 })
-app.listen(3000, () => {
-    console.log('listening on port 3000!!!!!!');
-});
+app.use(errorHandler);
+
+const start = async () => {
+
+    if(!process.env.JWT_KEY){
+        throw new Error('jwt secret key is not ');
+    }
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('connected to mongodb');
+    }catch(err) {
+        console.log(err);
+    }
+
+    app.listen(3000, async () => {
+        console.log('listening on port 3000!!!!!!!');
+    });
+}
+start();
