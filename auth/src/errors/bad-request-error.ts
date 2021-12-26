829042715f68556edc776bd8ca0ee163908a22ac
dbali00 <dbali00@bhnetwork.com>
2021-12-27 00:50:22 +0530

    test added for auth module

--- auth/src/errors/bad-request-error.ts
+++ auth/src/errors/bad-request-error.ts
@@ -7,7 +7,6 @@ export class BadRequestError extends CustomError {
         Object.setPrototypeOf(this, BadRequestError.prototype);
     }
     serializeErrors() {
-        console.log(this.message);
         return [{message: this.message}];
     }
 }
